02162f91a56cd69ef5f3c4bdd576ee0ea7a99f41
Vasilis Skarleas <vskarleas@icloud.com>
2026-05-18 13:21:18 -0400

    Updated package.xml for the visualization package

--- Code/src/visualization/setup.py
+++ Code/src/visualization/setup.py
@@ -4,19 +4,21 @@ package_name = 'visualization'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='2.3.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', []),
+        ('share/' + package_name + '/config', []),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='vskarleas',
     maintainer_email='vskarleas@icloud.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Visualization and control interface. Provides a Foxglove web UI to monitor the robot in real-time, view the planned path, and select navigation goals.',
+    license='MIT',
     extras_require={
         'test': [
             'pytest',
@@ -24,6 +26,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            # e.g.: 'goal_publisher = visualization.goal_publisher:main',
         ],
     },
 )
