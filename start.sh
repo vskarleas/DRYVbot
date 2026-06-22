6af6873c6a31158b2f5781cc49a1072391baff49
Liam <liam.frija-altarac.1@etsmtl.net>
2026-06-22 19:01:09 -0400

    Updated the start bash script

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
+# 1. Force the script to run from its own directory
+cd "$(dirname "$0")"
+
+# 2. Source the main Ubuntu ROS 2 installation
+source /opt/ros/humble/setup.bash
+
 cd Code
 rm -rf build/ install/ log/
 colcon build
